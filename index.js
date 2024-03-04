bf8c77d0ba3d4a76a0f0d37cb780ed87e2c3535d
BarkinSayin <barkinsayin@hotmail.com>
2024-03-04 18:48:28 +0300

    with ternary operator

--- index.js
+++ index.js
@@ -1,83 +1,126 @@
-//Tıklayarak çalışmasını sağlayan kod
+// //Tıklayarak çalışmasını sağlayan kod
 
-for (let i = 0; i < document.querySelectorAll(".drum").length; i++) {
-  document.querySelectorAll(".drum")[i].addEventListener("click", function(){
-    let buttonInnerHTML = this.innerHTML;
-  switch (buttonInnerHTML) {
-    case "w":
-      let tom1 = new Audio("sounds/tom-1.mp3");
-      tom1.play();
-      break;
-    case "a":
-      let tom2 = new Audio("sounds/tom-2.mp3");
-      tom2.play();
-      break;
-    case "s":
-      let tom3 = new Audio("sounds/tom-3.mp3");
-      tom3.play();
-      break;
-    case "d":
-      let tom4 = new Audio("sounds/tom-4.mp3");
-      tom4.play();
-      break;
-    case "j":
-      let snare = new Audio("sounds/snare.mp3");
-      snare.play();
-      break;
-    case "k":
-      let crash = new Audio("sounds/crash.mp3");
-      crash.play();
-      break;
-    case "l":
-      let kick = new Audio("sounds/kick-bass.mp3");
-      kick.play();
-      break;
+// for (let i = 0; i < document.querySelectorAll(".drum").length; i++) {
+//   document.querySelectorAll(".drum")[i].addEventListener("click", function(){
+//     let buttonInnerHTML = this.innerHTML;
+//   switch (buttonInnerHTML) {
+//     case "w":
+//       let tom1 = new Audio("sounds/tom-1.mp3");
+//       tom1.play();
+//       break;
+//     case "a":
+//       let tom2 = new Audio("sounds/tom-2.mp3");
+//       tom2.play();
+//       break;
+//     case "s":
+//       let tom3 = new Audio("sounds/tom-3.mp3");
+//       tom3.play();
+//       break;
+//     case "d":
+//       let tom4 = new Audio("sounds/tom-4.mp3");
+//       tom4.play();
+//       break;
+//     case "j":
+//       let snare = new Audio("sounds/snare.mp3");
+//       snare.play();
+//       break;
+//     case "k":
+//       let crash = new Audio("sounds/crash.mp3");
+//       crash.play();
+//       break;
+//     case "l":
+//       let kick = new Audio("sounds/kick-bass.mp3");
+//       kick.play();
+//       break;
 
-    default:
-      break;
+//     default:
+//       break;
+//   }
+//   });
+// }
+
+// //Tuşlarla çalışmasını sağlayan kod
+
+// document.addEventListener("keydown", function(event) {
+//     makeSound(event.key);
+//   });
+
+// function makeSound(key) {
+//   switch (key) {
+//     case "w":
+//       let tom1 = new Audio("sounds/tom-1.mp3");
+//       tom1.play();
+//       break;
+//     case "a":
+//       let tom2 = new Audio("sounds/tom-2.mp3");
+//       tom2.play();
+//       break;
+//     case "s":
+//       let tom3 = new Audio("sounds/tom-3.mp3");
+//       tom3.play();
+//       break;
+//     case "d":
+//       let tom4 = new Audio("sounds/tom-4.mp3");
+//       tom4.play();
+//       break;
+//     case "j":
+//       let snare = new Audio("sounds/snare.mp3");
+//       snare.play();
+//       break;
+//     case "k":
+//       let crash = new Audio("sounds/crash.mp3");
+//       crash.play();
+//       break;
+//     case "l":
+//       let kick = new Audio("sounds/kick-bass.mp3");
+//       kick.play();
+//       break;
+
+//     default:
+//       break;
+//   }
+// }
+
+//iki kodun daha temiz yazılımı
+
+function handleDrumAction(event) {
+    const keyPressed =
+      event.type === "click" ? this.innerHTML : event.key.toLowerCase();
+    switch (keyPressed) {
+      case "w":
+        let tom1 = new Audio("sounds/tom-1.mp3");
+        tom1.play();
+        break;
+      case "a":
+        let tom2 = new Audio("sounds/tom-2.mp3");
+        tom2.play();
+        break;
+      case "s":
+        let tom3 = new Audio("sounds/tom-3.mp3");
+        tom3.play();
+        break;
+      case "d":
+        let tom4 = new Audio("sounds/tom-4.mp3");
+        tom4.play();
+        break;
+      case "j":
+        let snare = new Audio("sounds/snare.mp3");
+        snare.play();
+        break;
+      case "k":
+        let crash = new Audio("sounds/crash.mp3"); 
+        crash.play();
+        break;
+      case "l":
+        let kick = new Audio("sounds/kick-bass.mp3");
+        kick.play();
+        break;
+  
+      default:
+        break;
+    }
   }
+  document.querySelectorAll(".drum").forEach(function (drum) {
+    drum.addEventListener("click", handleDrumAction);
   });
-}
-
-//Tuşlarla çalışmasını sağlayan kod
-
-document.addEventListener("keydown", function(event) {
-    makeSound(event.key);
-  });
-
-function makeSound(key) {
-  switch (key) {
-    case "w":
-      let tom1 = new Audio("sounds/tom-1.mp3");
-      tom1.play();
-      break;
-    case "a":
-      let tom2 = new Audio("sounds/tom-2.mp3");
-      tom2.play();
-      break;
-    case "s":
-      let tom3 = new Audio("sounds/tom-3.mp3");
-      tom3.play();
-      break;
-    case "d":
-      let tom4 = new Audio("sounds/tom-4.mp3");
-      tom4.play();
-      break;
-    case "j":
-      let snare = new Audio("sounds/snare.mp3");
-      snare.play();
-      break;
-    case "k":
-      let crash = new Audio("sounds/crash.mp3");
-      crash.play();
-      break;
-    case "l":
-      let kick = new Audio("sounds/kick-bass.mp3");
-      kick.play();
-      break;
-
-    default:
-      break;
-  }
-}
-
+  document.addEventListener("keydown", handleDrumAction);
